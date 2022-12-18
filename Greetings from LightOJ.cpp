e432b45affcf57babc235614808e24cdbe13d972
Asfak Shahrier <59991886+asfak27@users.noreply.github.com>
2022-12-18 19:24:18 +0600

    Create Greetings from LightOJ.cpp

---
+++ Greetings from LightOJ.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll n,i,j,c=0,k,x,y,a,b,t;
+    cin>>t;
+    while(t--)
+    {
+        c++;
+        cin>>a>>b;
+        x=a+b;
+        cout<<"Case "<<c<<": "<<x<<endl;
+    }
+}
